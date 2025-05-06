1907d8bdf3a308d67b02519fd3365637f321a094
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2025-05-06 10:35:19 +0700

    membuat prosedur input pada kelas persegipanjang dengan akses public

--- contoh.cpp
+++ contoh.cpp
@@ -4,4 +4,13 @@ using namespace std;
 class PersegiPanjang{
     public :
     int panjang, lebar;
+
+    public :
+    void input(){
+        cout << "Masukkan panjang : ";
+        cin >> panjang;
+        cout << "masukkan lebar : ";
+        cin >> lebar;
+    }
+
 }
