6f82722545b5f8518daf911bd501e405b2c73bd2
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2025-05-06 11:24:07 +0700

    membuat kelas mahasiswa dan menahmbahkan fungsi void menggunakan friend

---
+++ friendFunctionSatu.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+    private:
+    string nama;
+    public : 
+    friend void setNama(mahasiswa&a, string b);
+
+};
+
+void setNama(mahasiswa &a, string b){
+    a.nama = b ;
+    cout << a.nama;
+}
+
